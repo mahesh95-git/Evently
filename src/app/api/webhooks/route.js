ca638e485bc8aa5143546b1ed27d76e460210a7e
maheshRathod <mahesh956154@gmail.com>
2024-06-19 14:58:22 +0530

    fix delete user bug

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { createUser, updateUser } from "@/lib/actions/user.action";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -75,14 +75,20 @@ export async function POST(req) {
       photo: image_url,
     };
     // Add user to database
-    const newUser = await createUser(user);
-    if (newUser) {
+    try {
+      const newUser = await createUser(user);
+      if(newUser){
       clerkClient.users.updateUser(id, {
         publicMetadata: {
           userId: newUser._id,
         },
       });
     }
+    } catch (error) {
+      console.log(error);
+    }
+    
+    console.log(newUser);
     return NextResponse.json(
       { success: true, user: newUser },
       {
